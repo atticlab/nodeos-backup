48e682afa216ad99ce39affd31863dffd9e3cd52
darkeyenick <darkeyenick@gmail.com>
2019-01-30 18:55:56 +0200

    change backup.sh

--- backup.sh
+++ backup.sh
@@ -34,11 +34,11 @@ mkdir $BACKUPDIR
 
 if [ -d "$BACKUPDIR" ]; then
      cp -rf $DATADIR/state/ ${BACKUPDIR}/ && cp -rf $DATADIR/blocks/ ${BACKUPDIR}/
-     echo -e "Starting Nodeos \n";
-     ulimit -c unlimited
-     ulimit -n 65535
-     ulimit -s 64000
-     $NODEOSBINDIR/nodeos/nodeos --data-dir $DATADIR --config-dir $DATADIR > $DATADIR/stdout.txt 2> $DATADIR/stderr.txt &  echo $! > $DATADIR/nodeos.pid
+     #echo -e "Starting Nodeos \n";
+     #ulimit -c unlimited
+     #ulimit -n 65535
+     #ulimit -s 64000
+     #$NODEOSBINDIR/nodeos/nodeos --data-dir $DATADIR --config-dir $DATADIR > $DATADIR/stdout.txt 2> $DATADIR/stderr.txt &  echo $! > $DATADIR/nodeos.pid
 else
    echo $BACKUPDIR is not created. Check your permissions.
    exit 0
